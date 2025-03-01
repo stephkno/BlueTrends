79bbcc417f0f8dd5f32e58ccd281d2d5fdd68f99
stephen <stephkno@gmail.com>
2025-03-01 14:21:01 -0800

    Clean up old files

--- test.js
+++
@@ -1,9 +0,0 @@
-let hashtags = ["#one", "#two", "#three #more #words #here", "#four https://www.com", "#five", "#Pendejo 💙#Swineocracy💙 #ElonMusk #gop",
-    "#gntm Ad:"
-]
-
-hashtags = hashtags.map(hashtag => {
-    return hashtag.split(" ")[0];
-})
-
-console.log(hashtags);
